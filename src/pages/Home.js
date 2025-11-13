b467982c44c9cced6ae67e6c6e00d6f27a449ad0
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2025-11-13 20:59:53 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -191,9 +191,9 @@ const Home = () => {
           <div className="max-w-6xl mx-auto">
             <div className="rounded-2xl overflow-hidden shadow-2xl">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.2825956587124!2d77.19013631508078!3d28.613939382422254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1d6a7a1a8b8b%3A0x2e3f0a1e6f8e0e0e!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1234567890123!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3322.584878028413!2d75.60040687517976!3d27.341637876394095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396cfbeacd0e54a1%3A0x8147bee278bfcd49!2sJhankar%20Hotel!5e1!3m2!1sen!2sin!4v1763043721110!5m2!1sen!2sin"
                 width="100%" 
-                height="500" 
+                height="500"  
                 style={{ border:0 }} 
                 allowFullScreen="" 
                 loading="lazy" 
@@ -253,5 +253,6 @@ const GalleryItem = ({ img, title, desc }) => (
     </div>
   </div>
 );
+
 
 export default Home;
